a246a846889bf984e4032e4a442f0a0fd37bc1f5
Damian Alanis <dalanisr@cisco.com>
2021-11-06 22:45:33 -0600

    ESAStatus and Mailer

---
+++ esalib/esa_utils/ESARemediationStatus.py
@@ -0,0 +1,128 @@
+
+class ESABaseRemediationStatusCodes():
+    """
+    @version 1.2.0
+
+    Container of the status codes for the remediation and their corresponding message.
+    It can be extended with more definitions.
+    """
+    # Codes
+    ERROR = 100
+    SUCCESS = 200
+
+    # Messages
+    messages: dict[int, str] = {
+        ERROR: 'There was an error: <%s>',
+        SUCCESS: 'The issue was solved successfully, all the steps were executed correctly.',
+    }
+
+    def append_messages(self, messages_dictionary: dict[int, str]):
+        """
+        @param {dict} messages_dictionary The dictionary containing the custom messages.
+
+        Method to merge the initial dictionarey with the custom one.
+        """
+        self.messages = { **self.messages, **messages_dictionary }
+
+    def get_message_by_status_code(self, status_code: int) -> str:
+        """
+        @param {int} status_code Status code whose message we want to retrieve.
+
+        Method to retrieve the message for a certain status code.
+        """
+        return self.messages[status_code] if status_code in self.messages.keys() else ''
+
+
+class ESARemediationStatus():
+    """
+    @version 1.2.0
+
+    Class to keep track of the remediation status via status codes. It also allows to keep track of the 
+    files that were modified, which are candidates to be incorporated to the mail that is sent at the end
+    of the process. 
+    """
+
+    def __init__(
+        self, 
+        custom_status_codes: ESABaseRemediationStatusCodes = None
+    ):
+        """
+        @param {ESABaseRemediationStatusCodes} custom_status_codes Extended definition of status codes with custom messages and code numbers.
+        """
+        self.files: list[str] = []
+        self.messages = { }
+        self.custom_status_codes = custom_status_codes if custom_status_codes != None else ESABaseRemediationStatusCodes()
+
+    # Setters
+
+    def push_status_code(self, status_code: int):
+        """
+        @param {int} status_code Remediation status code.
+
+        Appends a status code to the dictionary.
+        """
+        self.__add_code_to_dictionary(status_code)
+
+    def push_obtained_file(self, file_name: str):
+        """
+        @param {str} file_path The path to the retrieved file.
+
+        Appends an obtained file path to the list.
+        """
+        self.__add_file_to_list(file_name)
+
+    def push_uploaded_file(self, file_path: str):
+        """
+        @param {str} file_path The path to the uploaded file.
+
+        Appends an uploaded (and therefore, modified) file path to the list.
+        """
+        self.__add_file_to_list(file_path)
+    
+    def set_error_message(self, error_message: str):
+        """
+        @param {str} error_message Error message string.
+
+        Sets the specific error message to incorporate, it will fill the missing part of the generic error message.
+        """
+        error_key = ESABaseRemediationStatusCodes.ERROR
+        self.messages[error_key] = self.custom_status_codes.get_message_by_status_code(error_key) % error_message
+
+    # Facade
+
+    def get_remediation_messages(self):
+        """
+        Returns a string with all the remediation messages joined by new line characters.
+        Suitable for email body.
+        """
+        return '\n'.join(value for value in self.messages.values())
+
+    def get_remediation_attachments(self) -> list[str]:
+        """
+        Returns the list of file paths that were retrieved or updated during the remediation process.
+        Suitable for email attachments.
+        """
+        return self.files
+
+
+    # Private methods
+
+    def __add_file_to_list(self, file_path: str):
+        """
+        @param {str} file_path Path to the file to add.
+
+        Method to push a non-existing file path to the list.
+        """
+        # If the file path already exists, we skip it
+        if file_path in self.files:
+            return
+        self.files.append(file_path)
+        
+
+    def __add_code_to_dictionary(self, status_code: int):
+        """
+        @param {int} status_code Remediation status code to add.
+
+        Method to add a code to the dictionary.
+        """
+        self.messages[status_code] = self.custom_status_codes.get_message_by_status_code(status_code)
---
+++ esalib/utils/mail/CaseMailer.py
@@ -0,0 +1,84 @@
+# Email manager
+from ...infrastructure.email_manager.EmailManager import EmailManager
+# ESA utils
+from ...esa_utils.ESARemediationStatus import ESARemediationStatus
+# Utils
+from ...utils.logger.Logger import Logger
+
+
+class CaseMailer:
+    """
+    @version 1.1.1
+
+    Class that encapsulates the process and values involved in the process of sending the informative
+    mail with the remediation status.
+    It makes use of the EmailManager class to easily send a mail, by providing the most elemental data,
+    such as the case owner email, and the remediation status (from which the email text body and attachments
+    are obtained via the get_remediation_messages() and get_remediation_attachments() respectively).
+    """
+
+    # SMTP server domain
+    __SMTP_SERVER = 'outbound.cisco.com'
+
+    # Email subject helper string, it is completed with the cas number
+    __EMAIL_SUBJECT = 'SR:%s'
+
+    # Default email body (to indicate a general error)
+    __DEFAULT_EMAIL_BODY = 'There was an error, please review the attached log file.'
+
+    def __init__(
+        self, 
+        esa_status: ESARemediationStatus,
+        case_owner_email: str,
+        case_identification_number: str
+    ):
+        """
+        @param {ESARemediationStatus} esa_status Instance that contains the status of the remediation, as well as the record of the modified files.
+        @param {str} case_ownr_email Email address of the case owner.
+        @param {str} case_identification_number ID of the case (SR).
+        """
+        self.esa_status = esa_status
+        self.case_owner_email = case_owner_email
+        self.case_identification_number = case_identification_number
+
+    def send_mail(self):
+        """
+        Method to send the mail, using the EmailManager class. It only requires all the data that
+        we already have, such as the SMTP server domain, the sender (from), target (to), subject,
+        email body and attachments. These last 2 are obtained from the ESA remediation status instance.
+        """
+        # We compose the subject with the case number
+        subject = self.__EMAIL_SUBJECT % self.case_identification_number
+        # We send the mail via the EmailManager
+        Logger.info(f'Sending email to { self.case_owner_email } with the remediation details...')
+        EmailManager(
+            server = self.__SMTP_SERVER,
+            sender = self.case_owner_email,
+            target = self.case_owner_email,
+            subject = subject,
+            email_body = self.__get_email_body(),
+            attachments = self.__get_email_attachments()
+        ).send()
+        Logger.info('Email sent successfully!')
+
+    def __get_email_body(self):
+        """
+        Method to get the email body from the ESARemediationStatus instance, if available, otherwise, we send a default message
+        to indicate a general error.
+        """
+        return ( 
+            self.esa_status.get_remediation_messages() 
+            if self.esa_status != None
+            else self.__DEFAULT_EMAIL_BODY
+        )
+
+    def __get_email_attachments(self):
+        """
+        Method to get the rerieved files during the process from the ESARemediationStatus instance, if available, otherwise, we send 
+        the own log file that contains the general error.
+        """
+        return (
+            self.esa_status.get_remediation_attachments()
+            if self.esa_status != None
+            else [Logger.output_log_file_name]
+        )
