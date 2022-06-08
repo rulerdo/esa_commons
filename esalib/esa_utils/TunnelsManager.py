731832a00d6423b96cce9135396fd11116966831
Daniela <dvignaul@cisco.com>
2022-06-08 14:43:13 -0500

    Add TunnelsCLIArguments class to read arguments from the command line and set them in the TunnelsManager class

--- esalib/esa_utils/TunnelsManager.py
+++ esalib/esa_utils/TunnelsManager.py
@@ -2,6 +2,10 @@
 from .ESASSHAgent import ESASSHAgent
 from .ESAParameters import LoggerInitializer, ESASSHParameters
 
+import argparse
+import os
+from collections import namedtuple
+
 class TunnelsManager:
 
     def __init__(self):
@@ -11,6 +15,7 @@ class TunnelsManager:
         # To be defined later
         self.ssh_agent: ESASSHAgent = None
         self.ssh_parameters: ESASSHParameters = None
+        self.tunnels_parmeters: TunnelsCLIArguments = None
 
     def initialize(self):
         """Facade to retrieve the SSH parameters and start the connection."""
@@ -19,19 +24,16 @@ class TunnelsManager:
         self.start_connection()
 
     def set_ssh_parameters(self):
-        """Retrieves the parameters for the SSH connection to the Tunnels."""
-        tunnels_ip = input('Enter the Tunnels IP address or domain: ')
-        tunnels_user = input('Enter the SSH user: ')
-        tunnels_password = input('Enter the SSH password: ')
-        # We set the custom SSH port if provided
-        tunnels_ssh_port_custom = input('Enter the Tunnels SSH port [22]: ')
-        tunnels_ssh_port = tunnels_ssh_port_custom if tunnels_ssh_port_custom else 22
         # We create the SSHParameters object
+        tunnels_ip = self.tunnels_parmeters.params.tunnels_host
+        tunnels_user = self.tunnels_parmeters.params.user
+        tunnels_password = self.tunnels_parmeters.params.password
+
         self.ssh_parameters = ESASSHParameters(
             esa_ip = tunnels_ip,
             esa_user = tunnels_user,
             esa_password = tunnels_password,
-            esa_ssh_port = tunnels_ssh_port
+            esa_ssh_port = 22
         )
 
     def start_connection(self):
@@ -48,7 +50,76 @@ class TunnelsManager:
         return self.ssh_agent
 
     def set_esa_credentials(self):
-        serial_number = input('Enter the ESA serial number: ')
-        seed_id = input('Enter the seed: ')
-        self.credentials = (serial_number, seed_id)
-        self.command_prefix = f'tunnels -L {serial_number} -p {seed_id}'
+        serial_number = self.tunnels_parmeters.params.serial_number
+        seed = self.tunnels_parmeters.params.seed
+
+        self.credentials = (serial_number, seed)
+        self.command_prefix = f'tunnels -L {serial_number} -p {seed}'
+
+    def get_file_tunnels(self,file,path):
+        abs_filename = path + file
+
+        print(f'Downloading {file} from ESA...')
+        output = self.ssh_agent.execute_command(f'echo { self.command_prefix } -G {abs_filename}')
+        print(output)
+
+        print(f'Copying {file} to local host')
+        self.ssh_agent.get_file_with_scp(file)
+        print('File copied from ESA -> Tunnels and from Tunnels --> Host')
+
+
+    def put_file_tunnels(self,file,path):
+        abs_filename = path + file
+
+        print(f'Uploading {file} to Tunnels...')
+        self.ssh_agent.upload_file_with_scp(file, '.')
+        print(f'{file} uploaded!')
+
+        print(f'Uploading {file} to ESA...')
+        output = self.ssh_agent.execute_command(f'echo { self.command_prefix } -P {abs_filename}')
+        print(output)
+        print(f'{file} uploaded to /tmp on ESA')
+
+        print(f'Connecting to ESA terminal...')
+        output = self.ssh_agent.execute_command(f'echo { self.command_prefix } -C')
+        print(output)
+
+        print(f'Moving {file} to final location...')
+        output = self.ssh_agent.execute_command(f'echo "mv /tmp/{file} {abs_filename}"')
+        print(output)
+
+    def cleanup_temp_file_tunnels(self,file,local=False):
+        print(f'Cleaning up {file} temp copies')
+
+        # Delete file on Tunnels server
+        output = self.ssh_agent.execute_command(f'echo "rm {file}"')
+        print(output)
+
+        # Delete local file
+        if local:
+            os.remove(file)
+
+        print(f'{file} deleted from tunnels and local host')
+
+class TunnelsCLIArguments(argparse.ArgumentParser):
+
+    def __init__(self):
+        super().__init__()
+        self.__add_tunnel_arguments()
+        self.params = self.__get_tunnels_arguments()
+
+    def __add_tunnel_arguments(self):
+        self.add_argument('-t', '--tunnels-host', help = 'IP or hostname of Tunnel Server')
+        self.add_argument('-u', '--user', help = 'User on server')
+        self.add_argument('-p', '--password', help = 'Password on server')
+        self.add_argument('-n', '--serial-number', help = 'Serial number of the ESA')
+        self.add_argument('-s', '--seed', help = 'Seed used to connect to the ESA')
+
+    def __get_tunnels_arguments(self):
+        args = self.__add_tunnel_arguments().arguments
+
+        Tunnels = namedtuple('Tunnel parameters', 'tunnels_host user password serial_number seed')
+
+        self.params = Tunnels(args.tunnels_host, args.user, args.password, args.serial_number, args.seed)
+
+        return self.params
